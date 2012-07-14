e216037143927613d34ff707acc8d2cdb1478c7c
Marcelo Gomes Barbosa <marcelo.esk@hotmail.com>
2012-07-14 15:52:12 -0400

    Inicio

---
+++ src/main/java/ged/domain/documentos/Localizador.java
@@ -0,0 +1,13 @@
+package ged.domain.documentos;
+
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(table = "localizador", schema = "documentos")
+@RooDbManaged(automaticallyDelete = true)
+public class Localizador {
+}
---
+++ src/main/java/ged/domain/documentos/Pastas.java
@@ -0,0 +1,13 @@
+package ged.domain.documentos;
+
+import org.springframework.roo.addon.dbre.RooDbManaged;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(table = "pastas", schema = "documentos")
+@RooDbManaged(automaticallyDelete = true)
+public class Pastas {
+}
